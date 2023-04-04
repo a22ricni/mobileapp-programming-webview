befa9de2409c9c91fced834d94adc667adff21d5
a22ricni <a22ricni@student.his.se>
2023-04-04 11:03:30 +0200

    Added webviewClient
    Added the webviewClient and needed to import the webviewclient.

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         WebView myWebView = findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
         setSupportActionBar(toolbar);
 
         /*
