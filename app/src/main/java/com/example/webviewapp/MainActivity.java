6764e23b3e7a352762bde41876d2727866731925
a22ricni <a22ricni@student.his.se>
2023-04-04 11:41:07 +0200

    url in the other classes
    I added other url on the class showExternalPage and showInteralPage

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -11,12 +11,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
     public void showExternalWebPage(){
-        // TODO: Add your code for showing external web page here
+        myWebView = findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://github.com/a22ricni/mobileapp-programming-webview/commits/master");
     }
 
     public void showInternalWebPage(){
-        // TODO: Add your code for showing internal web page here
+        myWebView = findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://github.com/LenaSYS/mobileapp-programming-webview/tree/master/app/src/main/res");
     }
 
     @Override
